33e8476bff92dbcd8db0e73517007d9b3d9d1771
dannycastilloo <dannycastillootiniano@gmail.com>
2023-05-13 13:12:10 -0500

    Initial Commit

---
+++ semana7-b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  semana7-b
+//
+//  Created by MAC37 on 13/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
